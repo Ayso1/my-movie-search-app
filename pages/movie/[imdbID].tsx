2539124611d68bb0d4e6aed9c24560c663b0bcca
Ayso1 <reznichenko.max9@gmail.com>
2023-03-10 12:46:13 +0100

    Movie page loading fix

--- pages/movie/[imdbID].tsx
+++ pages/movie/[imdbID].tsx
@@ -23,6 +23,7 @@ type MovieProps = {
 export default function MoviePage() {
   const [data, setData] = useState(Object);
   const [loading, setLoading] = React.useState(false);
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
 
   const allData = async () => {
     setLoading(true);
@@ -41,60 +42,63 @@ export default function MoviePage() {
     setLoading(false);
   };
   useEffect(() => {
+    setIsDataLoaded(true);
     allData();
   }, []);
   return (
     <main className={styles.main}>
-      <Card
-        className={styles.card}
-        orientation="horizontal"
-        sx={{ backgroundColor: '#2E3B55', width: '80%' }}
-      >
-        <CardMedia
-          className={styles.media}
-          component="img"
-          image={data.Poster}
-          sx={{
-            width: '30%',
-            height: '40%',
-          }}
-          style={{ borderRadius: '8px' }}
-        />
+      {isDataLoaded && (
+        <Card
+          className={styles.card}
+          orientation="horizontal"
+          sx={{ backgroundColor: '#2E3B55', width: '80%' }}
+        >
+          <CardMedia
+            className={styles.media}
+            component="img"
+            image={data.Poster}
+            sx={{
+              width: '30%',
+              height: '40%',
+            }}
+            style={{ borderRadius: '8px' }}
+          />
 
-        <CardContent sx={{ px: 12 }}>
-          <Typography sx={{ mb: 2, color: '#000000' }} variant="h4">
-            {data.Title}
-          </Typography>
-          <Typography sx={{ mb: 2, color: '#000000' }} variant="body1">
-            <strong>Release Date: </strong>
-            {data.Released}
-          </Typography>
-          <Typography sx={{ mb: 2, color: '#000000' }} variant="body1">
-            <strong>Country: </strong>
-            {data.Country}
-          </Typography>
-          <Typography sx={{ mb: 2, color: '#FFFF2E' }} variant="body1">
-            <strong>Rated: </strong>
-            {data.Rated}
-          </Typography>
-          <Typography sx={{ mb: 2, color: '#000000' }} variant="body1">
-            <strong>Genre: </strong>
-            {data.Genre}
-          </Typography>
-          <Typography sx={{ mb: 2, color: '#000000' }} variant="body1">
-            <strong>Director: </strong>
-            {data.Director}
-          </Typography>
-          <Typography sx={{ mb: 2, color: '#000000' }} variant="body1">
-            <strong>Actors: </strong>
-            {data.Actors}
-          </Typography>
-          <Typography sx={{ mb: 2, color: '#000000' }} variant="body1">
-            <strong>Plot: </strong>
-            {data.Plot}
-          </Typography>
-        </CardContent>
-      </Card>
+          <CardContent sx={{ px: 12 }}>
+            <Typography sx={{ mb: 2, color: '#000000' }} variant="h4">
+              {data.Title}
+            </Typography>
+            <Typography sx={{ mb: 2, color: '#000000' }} variant="body1">
+              <strong>Release Date: </strong>
+              {data.Released}
+            </Typography>
+            <Typography sx={{ mb: 2, color: '#000000' }} variant="body1">
+              <strong>Country: </strong>
+              {data.Country}
+            </Typography>
+            <Typography sx={{ mb: 2, color: '#FFFF2E' }} variant="body1">
+              <strong>Rated: </strong>
+              {data.Rated}
+            </Typography>
+            <Typography sx={{ mb: 2, color: '#000000' }} variant="body1">
+              <strong>Genre: </strong>
+              {data.Genre}
+            </Typography>
+            <Typography sx={{ mb: 2, color: '#000000' }} variant="body1">
+              <strong>Director: </strong>
+              {data.Director}
+            </Typography>
+            <Typography sx={{ mb: 2, color: '#000000' }} variant="body1">
+              <strong>Actors: </strong>
+              {data.Actors}
+            </Typography>
+            <Typography sx={{ mb: 2, color: '#000000' }} variant="body1">
+              <strong>Plot: </strong>
+              {data.Plot}
+            </Typography>
+          </CardContent>
+        </Card>
+      )}
     </main>
   );
 }
